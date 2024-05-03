0d039cea61a1ae2006be9990e00441a4988c956b
Alex Elgueta <alexelgueta.developer@gmail.com>
2024-05-02 22:42:18 -0400

    chore: :memo: Update useInfiniteImages to use environment variables for API URL, limit, sort, period, and nsfw options

--- src/hooks/useInfiniteImages.ts
+++ src/hooks/useInfiniteImages.ts
@@ -1,14 +1,16 @@
 import { useInfiniteQuery } from "react-query";
 import axios from "axios";
 
+
 const fetchImages = async ({ pageParam = null }) => {
-  const endpoint = `https://civitai.com/api/v1/images`;
+  const endpoint =
+    import.meta.env.VITE_API_URL || `https://civitai.com/api/v1/images`;
   const params = {
-    limit: 20,
+    limit: import.meta.env.VITE_LIMIT || 20,
     cursor: pageParam,
-    sort: "Most Reactions",
-    period: "Week",
-    nsfw: "None",
+    sort: import.meta.env.VITE_SORT || "Most Reactions",
+    period: import.meta.env.VITE_PERIOD || "Week",
+    nsfw: import.meta.env.VITE_NSFW || "None",
   };
 
   const response = await axios.get(endpoint, { params });
